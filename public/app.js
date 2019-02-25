f3f7c2b361a3de8a0e6125feda9bec1eb47ebd14
[verna100] <vernalizg@gmail.com>
2019-02-25 18:29:17 -0500

    heroku push

--- public/app.js
+++ public/app.js
@@ -3,13 +3,13 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < 5; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + data[i].summary + "</p>");
+    $("#articles").append("<button data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + data[i].summary + "</p>");
   }
 });
 
 
 // Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
+$(document).on("click", "add note", function() {
   // Empty the notes from the note section
   $("#notes").empty();
   // Save the id from the p tag
